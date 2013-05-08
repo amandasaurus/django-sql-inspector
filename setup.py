44d4ee70217a0b37b08cc625d0251ae7e8884d18
Rory McCann <rory@technomancy.org>
2013-05-08 11:48:20 +0100

    setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+#! /usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(name="django-sql-inspector",
+      version="0.1",
+      author="Rory McCann",
+      author_email="rory@technomancy.org",
+      py_modules=['sql_inspector'],
+      license = 'GPLv3',
+      description = 'x',
+)
