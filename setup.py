f4c03a2c7e6a49c2abf540499b237a257da7cdcb
Rory McCann <rory@technomancy.org>
2014-05-01 16:52:40 +0100

    Version bumb to 1.0.0 We'll use semver now

--- setup.py
+++ setup.py
@@ -4,11 +4,11 @@ from setuptools import setup, find_packages
 
 
 setup(name="django-sql-inspector",
-      version="0.1",
+      version="1.0.0",
       author="Rory McCann",
       author_email="rory@technomancy.org",
       packages=['sql_inspector'],
       license = 'GPLv3',
       description = 'Analyze and measure the SQL calls, used by your Django application',
-      requires=[ 'django' ],
+      requires=[ 'django>=1.4' ],
 )
